db0b0e831c90e5fbdc6d49553f074d0ecc83e991
TusharDahiyaa <66515549+TusharDahiyaa@users.noreply.github.com>
2020-07-19 18:11:01 +0530

    Add files via upload

---
+++ ghost.js
@@ -0,0 +1,36 @@
+var myAudio = new Audio('Bfg_Division.mp3');
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function changeHeaderColour(){
+  colorInput=getRandomColor();
+  $("#game-mode").css("color",colorInput);
+}
+
+setInterval("changeHeaderColour()",1500);
+
+$("#game-mode").on("click",function(){
+  myAudio.play();
+  $("#game-mode").html("<h1>GAME MODE <br>ACTIVATED!🎮</h1>");
+});
+
+$("#gameVideo").on("mouseover",function(){
+    myAudio.pause();
+});
+
+$("#gamerVideo").on("mouseover",function(){
+    myAudio.pause();
+});
+
+myAudio.addEventListener('pause', function(){
+  $(document).on("click",function(){
+    myAudio.play();
+  });
+});
